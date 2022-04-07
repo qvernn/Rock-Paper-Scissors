e306336231106f985ace72007731740ed61e2793
Carl Yeung <cmyeung@up.edu.ph>
2022-04-07 20:31:53 +0800

    Create function for prompting player hand selection

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function computerPlay() {
     compHand = select[Math.floor(Math.random()*select.length)];
     return compHand;
 }
+
+function playerPlay() {
+    playerChoose = prompt("Rock, Paper, or Scissors?",'');
+    playerChoose = playerChoose.charAt(0).toUpperCase + playerChoose.slice(1);
+    return playerChoose;
+}
+
+
+const playerSelection = playerPlay();
+const computerSelection = computerPlay();
