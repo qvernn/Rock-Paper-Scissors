662dcdbd8134e805cf5df9ee8f25d643168f198a
Carl Yeung <cmyeung@up.edu.ph>
2022-04-29 22:30:22 +0800

    Changed the UIbased on player selection

--- script.js
+++ script.js
@@ -12,23 +12,23 @@ function playRound(playerSelection,computerSelection) {
         if (computerSelection === 'Rock') {
             outcome = `You tied!`;
         } else if (computerSelection === 'Paper') {
-            outcome = `You lose! ${computerSelection} beats ${playerSelection}.`;
+            outcome = `You lose!`;
         } else if (computerSelection === 'Scissors') {
-            outcome = `You win! ${playerSelection} beats ${computerSelection}.`;
+            outcome = `You win!`;
         }
     } else if (playerSelection === 'Paper') {
         if (computerSelection === 'Rock') {
-            outcome = `You win! ${playerSelection} beats ${computerSelection}.`;
+            outcome = `You win!`;
         } else if (computerSelection === 'Paper') {
             outcome = `You tied!`;
         } else if (computerSelection === 'Scissors') {
-            outcome = `You lose! ${computerSelection} beats ${playerSelection}.`;
+            outcome = `You lose!`;
         }
     } else if (playerSelection === 'Scissors') {
         if (computerSelection === 'Rock') {
-            outcome = `You lose! ${computerSelection} beats ${playerSelection}.`;
+            outcome = `You lose!`;
         } else if (computerSelection === 'Paper') {
-            outcome = `You win! ${playerSelection} beats ${computerSelection}.`;
+            outcome = `You win!`;
         } else if (computerSelection === 'Scissors') {
             outcome = `You tied!`;
         }
@@ -39,13 +39,34 @@ function playRound(playerSelection,computerSelection) {
 function game() {
     const buttons = document.querySelectorAll('button');
     const playerHand = document.querySelector('.player-hand > .hand');
+    const roundResult = document.querySelector('.round-result');
+
+    const playerScore = document.querySelector('.player-score > .score');
+    playerScore.textContent = 0;
+    const computerScore = document.querySelector('.computer-score > .score');
+    computerScore.textContent = 0;
+
     buttons.forEach((button) => {
         button.addEventListener('click',(e) => {
             playerChoice = e.path[0].textContent;
             playerHand.textContent = playerChoice;
+
             computerChoice = computerPlay();
-            console.log(playerChoice);
-            console.log(computerChoice);
+
+            roundResult.textContent = playRound(playerChoice,computerChoice)
+            console.log(roundResult.textContent.includes('win'));
+
+            if (roundResult.textContent.includes('win')) {
+                playerScore.textContent++;
+            } else if (roundResult.textContent.includes('lose')) {
+                computerScore.textContent++;
+            }
+
+            if (playerScore.textContent == 5) {
+                console.log('You won against the computer!')
+            } else if (computerScore.textContent == 5) {
+                console.log('You lost against the computer.')
+            }
         })
     });
 }
